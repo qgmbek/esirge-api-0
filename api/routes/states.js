e16655c5671bda12e1d0479d8c65b314ab6a9037
Qogambek <100270972+qgmbek@users.noreply.github.com>
2024-05-12 13:21:35 +0300

    Update states.js

--- api/routes/states.js
+++ api/routes/states.js
@@ -24,7 +24,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 1024 * 1024 * 5
+    fileSize: 4096 * 4096 * 15
   },
   fileFilter: fileFilter
 });
